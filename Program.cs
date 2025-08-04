1b73000e65f47886c162d4267bb157dfb7cefb36
Zvonimir Belina <zbelina20@foi.hr>
2025-08-04 12:32:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add CORS for React app
+builder.Services.AddCors(options => {
+    options.AddPolicy("ReactApp", policy => {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("ReactApp");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
